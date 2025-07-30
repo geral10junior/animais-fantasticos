c1400da02f40eaab6e590da15c32fc2c5491dc4d
Geraldez Ribeiro <geraldezjunior09@gmail.com>
2025-07-30 19:09:19 -0300

    scroll suave adicionado

--- script.js
+++ script.js
@@ -40,3 +40,30 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initScrollToSection() {
+  const internalLinks = document.querySelectorAll(".js-menu a[href^='#']");
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const top = section.offsetTop;
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+
+    // forma alternativa
+    // window.scrollTo({
+    //   top: top,
+    //   behavior: "smooth",
+    // });
+  }
+
+  internalLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollToSection();
