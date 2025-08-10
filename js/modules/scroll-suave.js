c72a377c9b11a33710bd303978298dc78bd437a2
Geraldez Ribeiro <geraldezjunior09@gmail.com>
2025-08-10 18:18:32 -0300

    organização nos arquivos js

---
+++ js/modules/scroll-suave.js
@@ -0,0 +1,26 @@
+export default function initScrollToSection() {
+  const internalLinks = document.querySelectorAll(
+    "[data-menu='suave'] a[href^='#']"
+  );
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+    // forma alternativa
+    //  const top = section.offsetTop;
+    // window.scrollTo({
+    //   top: top,
+    //   behavior: "smooth",
+    // });
+  }
+
+  internalLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initScrollToSection from "./modules/scroll-suave.js";
+import initScrollAnimation from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+
+initScrollToSection();
+initScrollAnimation();
+initAccordion();
+initTabNav();
