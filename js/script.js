01e08f355a99d2f744c3fddf8a1548b497cb395f
Geraldez Ribeiro <geraldezjunior09@gmail.com>
2025-08-12 12:12:19 -0300

    tooltip

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initScrollAnimation from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
+import initToolTip from "./modules/tooltip.js";
 
 initScrollToSection();
 initScrollAnimation();
 initAccordion();
 initTabNav();
 initModal();
+initToolTip();
