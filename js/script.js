0b5f033083e356b260f3ff1ffca784f11d70bb73
Geraldez Ribeiro <geraldezjunior09@gmail.com>
2025-08-12 11:11:25 -0300

    modal

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrollToSection from "./modules/scroll-suave.js";
 import initScrollAnimation from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
 initScrollToSection();
 initScrollAnimation();
 initAccordion();
 initTabNav();
+initModal();
